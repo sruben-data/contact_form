e6775d36bc177ff8cc7e2de27049721f0de0c579
Sam Ruben <samuelruben123@gmail.com>
2026-05-20 08:35:47 -0700

    Finished P1 - pushing to remote to test GitHub Pages

--- main.py
+++ main.py
@@ -3,16 +3,30 @@
     [.venv/Scripts/activate]
     uv run main.py
 """
-import inspect
-from flask import Flask, json
+import inspect, os, logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+from flask import Flask, json, send_file
 from flask import request # global proxy object that intercepts the HTTP request
 from flask import Response
+# will need to import flask-cors at some point if you have browser talking to Flask app - adds CORS headers to response and allows cross-origin HTTP requests to your API
+# from flask-cors import CORS
+
+print('how many times is this file executed')
 
 app: Flask = Flask(__name__) # type annotation - app of type Flask
+app.logger.setLevel(logging.DEBUG)
 
 app.logger.debug(inspect.getsourcefile(Flask))
 app.logger.debug(inspect.getsourcefile(type(request)))
 app.logger.debug(inspect.getsourcefile(json))
+
+BASE_DIR = os.path.dirname(__file__)
+
+@app.route('/')
+def serve_form():
+    return send_file(os.path.join(BASE_DIR,'index.html'))
 
 @app.route('/submit', methods=['POST'])
 def handle_form_submission() -> Response:
@@ -22,11 +36,13 @@ def handle_form_submission() -> Response:
     data: dict = request.form.to_dict() # converts request.form MultiDict object (immutable) to regular dictionary
     app.logger.debug(type(request.form))
     app.logger.debug(inspect.getsourcefile(type(request.form)))
+    app.logger.debug(dir(request.form))
     app.logger.debug(inspect.getsourcefile(request.form.to_dict))
     app.logger.debug(type(data))
     app.logger.debug(dir(data))
     app.logger.debug(data)
 
+    # CORS Headers need to be applied here in cases where domain name for scriptURL does not match browser url (localhost != )
     # HTTP Response - manual reassignment of custom attributes
     resp = json.jsonify({'status': 'received', 'data': data}) # constructs Response object, serializes Python dict into JSON string using json.dumps, resp.set_data()
     # status = 201
@@ -51,4 +67,3 @@ def get_contact(contact_id: int) -> Response:
 
 if __name__ == '__main__':
     app.run(debug=True) # automatically loads from .env or .flaskenv file
-    
